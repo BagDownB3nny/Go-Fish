f12bc8dc1bb80ad9b6d6f5a469dad80a6b813b67
Darren Sim <e0725166@u.nus.edu>
2023-12-14 18:29:50 +0100

    Make frontend send correct catch post request

--- frontend/src/features/fishing/catch-fish.ts
+++ frontend/src/features/fishing/catch-fish.ts
@@ -4,7 +4,7 @@ import { FishSpecies } from "../../types/fish.types";
 export const catchFish = async (userId: string, bait : string) : Promise<FishSpecies>=> {
     const data = {
         userId,
-        bait
+        baitName: bait
     };
     try {
         const response = await api.post("/fish/catch-fish", data)
